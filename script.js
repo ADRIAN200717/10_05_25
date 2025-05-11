480f0e78045bab4f1a82ac40cf78fc1111cd5c37
ADRIAN200717 <61154814@certus.edu.pe>
2025-05-10 19:19:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function verificarNombre() {
+    let nombreIngresado = document.getElementById("nombreMama").value.trim().toLowerCase();
+    
+    
+    let nombresValidos = ["kathia", "kathia Romero", "kathia Elizabeth Romero Rodriguez", "kathia", "kathy"];
+
+    if (nombresValidos.includes(nombreIngresado)) {
+        window.location.href = "sorpresa.html"; 
+    } else {
+        alert("¡Hola! Parece que tu nombre no está registrado para esta sorpresa, igual deseo que pases un gran dia justo a tus seres queridos 💖");
+    }
+}
